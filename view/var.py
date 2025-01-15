952ad5eeffdd2ceeddd90cc955c373711acd7119
Prof Snape <95466549+ProfSnape07@users.noreply.github.com>
2025-01-16 02:53:10 +0530

    add credentials to .env

--- view/var.py
+++ view/var.py
@@ -1,4 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # Declaring some variable here to enable their changing easy.
-eth_provider = "https://rpc2.sepolia.org"
+eth_provider = os.getenv("ETH_PROVIDER")
+print(eth_provider)
 abi_file = "static/contract/contract.abi"
 contract_address = "0x40d2E6726F550ccD98d87F3d9df79E81cCd6f87e"
