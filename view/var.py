622691af19289051f5b28d7eaeaf65e7340a0ae6
Prof Snape <95466549+ProfSnape07@users.noreply.github.com>
2025-01-16 03:52:16 +0530

    remove print statement

--- view/var.py
+++ view/var.py
@@ -5,6 +5,5 @@ load_dotenv()
 
 # Declaring some variable here to enable their changing easy.
 eth_provider = os.getenv("ETH_PROVIDER")
-print(eth_provider)
 abi_file = "static/contract/contract.abi"
 contract_address = "0x40d2E6726F550ccD98d87F3d9df79E81cCd6f87e"
